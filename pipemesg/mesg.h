ba99338626c0c1d1c5d90efb89cff36e05d7da19
fengxc <1143734409@qq.com>
2022-10-31 21:22:44 +0800

    10/31 pipe&fifo

---
+++ pipemesg/mesg.h
@@ -0,0 +1,16 @@
+#include    "unpipc.h"
+
+#define MAXMESGDATA (PIPE_BUF - 2*sizeof(long))
+
+#define MESGHDRSIZE (sizeof(struct mymesg) - MAXMESGDATA)
+
+struct mymesg{
+    long    mesg_len;
+    long    mesg_type;
+    char    mesg_data[MAXMESGDATA];
+};
+
+ssize_t mesg_send(int, struct mymesg *);
+void    Mesg_send(int, struct mymesg *);
+ssize_t mesg_recv(int, struct mymesg *);
+ssize_t Mesg_recv(int, struct mymesg *);
