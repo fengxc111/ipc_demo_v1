826e47486495f8d084ae492385328ddea5a619af
fengxc <1143734409@qq.com>
2022-11-01 15:13:00 +0800

    11/01 posix message queue

---
+++ pxmsg/mqsysconf.c
@@ -0,0 +1,12 @@
+#include    "unpipc.h"
+
+int
+main(int argc, char **argv)
+{
+    printf("MQ_OPEN_MAX = %ld, MQ_PRIO_MAX = %ld\n",
+            Sysconf(_SC_MQ_OPEN_MAX), Sysconf(_SC_MQ_PRIO_MAX));
+    
+    // printf("_SC_MQ_OPEN_MAX = %d\n",_SC_MQ_OPEN_MAX);
+    // printf("_SC_MQ_PRIO_MAX = %d",_SC_MQ_PRIO_MAX);
+    exit(0);
+}
