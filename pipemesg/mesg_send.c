a90cee7591f82b72fabf0291a51e0795a38ed6f8
fengxc <1143734409@qq.com>
2022-11-01 08:53:39 +0800

    11/01 pipemesg/mesg_send rectify

--- pipemesg/mesg_send.c
+++ pipemesg/mesg_send.c
@@ -10,6 +10,8 @@ void
 Mesg_send(int fd, struct mymesg *mptr)
 {
     ssize_t n;
-    if ( (n = mesg_send(fd, mptr)) != mptr->mesg_len)
+    if ( (n = mesg_send(fd, mptr)) != mptr->mesg_len+MESGHDRSIZE){
+        // printf("n = %ld len = %ld", (long)n, (long)mptr->mesg_len);
         err_quit("mesg_send error");
+    }
 }
