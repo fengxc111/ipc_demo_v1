22265985fa069b5849750e3d39364664ab0c8143
fengxc <1143734409@qq.com>
2022-11-20 08:52:05 +0800

    11/20 Sun rpc incomplete

--- sunrpc/square4/client.c
+++ sunrpc/square4/client.c
@@ -5,4 +5,14 @@ int
 main(int argc, char **argv)
 {
     CLIENT  *cl;
+    square_in   in;
+    square_out  out;
+
+    if (argc != 3)
+        err_quit("usage: client <hostname> <integer-value>");
+    
+    cl = Clnt_create(argv[1], SQUARE_PROG, SQUARE_VERS, "tcp");
+
+    auth_destroy(cl->cl_auth);
+    cl->cl_auth = authsys_create_default();
 }
